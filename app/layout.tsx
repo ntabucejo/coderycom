8048c18588ef966f82ef6fc34a57dde0cdae0d99
ntabucejo <ntabucejo@gmail.com>
2022-12-16 13:28:26 +0800

    chore(style): setup tailwindcss, font family and paths

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,21 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+import "@core/styles/globals.css";
+import { Inter } from "@next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Layout = ({ children }: Props) => {
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <body className={`${inter.className}`}>{children}</body>
     </html>
-  )
-}
+  );
+};
+
+export default Layout;
