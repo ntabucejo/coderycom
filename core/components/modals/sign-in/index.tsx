939aae49b683039fab78abf181a18e536b6bfe06
ntabucejo <ntabucejo@gmail.com>
2022-12-17 22:16:44 +0800

    chore(sign-in): add sign in modal

---
+++ core/components/modals/sign-in/index.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Button from "@core/components/elements/button";
+import Modal from "@core/components/layouts/modal";
+import useModal from "@core/hooks/use-modal";
+import { signIn } from "next-auth/react";
+
+const SignIn = () => {
+  const { state, handleOpen, handleClose } = useModal();
+
+  return (
+    <>
+      <Button variant="secondary" onClick={handleOpen}>
+        Sign In
+      </Button>
+
+      <Modal
+        title="Welcome Back"
+        state={state}
+        handleClose={handleClose}
+        className="max-w-md">
+        <button
+          onClick={() => signIn("google")}
+          className="w-full rounded border py-2 px-4">
+          Google
+        </button>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+      </Modal>
+    </>
+  );
+};
+
+export default SignIn;
