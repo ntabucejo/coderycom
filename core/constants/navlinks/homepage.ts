aeaa6c613438f08f03e918f69db82d854e00e904
jazztine <jzztn.crz@gmail.com>
2022-12-19 23:43:43 +0800

    chore(constants): create constants folder and create homepage navlinks

---
+++ core/constants/navlinks/homepage.ts
@@ -0,0 +1,14 @@
+export const NavLinks = [
+  {
+    name: "Home",
+    href: "#",
+  },
+  {
+    name: "What's Popular",
+    href: "#",
+  },
+  {
+    name: "About Us",
+    href: "#",
+  },
+];
