28049cc959ec31d074b89ea4b3f22a47b35fb7c5
jazztine <jzztn.crz@gmail.com>
2022-12-19 23:40:00 +0800

    chore(navbar): create navbar for mobile and laptop

--- core/components/sections/navigation/index.tsx
+++ core/components/sections/navigation/index.tsx
@@ -1,16 +1,38 @@
+"use client";
+import Button from "@core/components/elements/button";
+import Logo from "@core/components/elements/logo";
+import Dropdown from "@core/components/layouts/dropdown";
 import SignIn from "@core/components/modals/sign-in";
+import { NavLinks } from "@core/constants/navlinks/homepage";
+import useDropdown from "@core/hooks/use-dropdown";
 import Route from "./route";
 import Search from "./search";
 
 const Navigation = () => {
+  const { state, handleClose, handleOpen } = useDropdown();
+
   return (
-    <nav className="flex items-center gap-10 text-sm">
+    <nav className="contain flex items-center gap-2 text-sm tablet:gap-4 laptop:gap-10">
+      <Logo />
       <Search />
-      <ul className="flex items-center gap-6">
-        <Route name="Home" href="/" />
-        <Route name="What's Popular" href="/" />
-        <Route name="About Us" href="/" />
-      </ul>
+      {/* nav links for mobile */}
+      <div className="tablet:hidden">
+        <Dropdown links={NavLinks} state={state} handleClose={handleClose}>
+          <Button variant="primary" onClick={handleOpen}>
+            Menu
+          </Button>
+        </Dropdown>
+      </div>
+
+      {/* nav links for laptop */}
+      <div className=" book:hidden tablet:block">
+        <ul className="flex items-center gap-6">
+          <Route name="Home" href="/" />
+          <Route name="What's Popular" href="/" />
+          <Route name="About Us" href="/" />
+        </ul>
+      </div>
+
       <SignIn />
     </nav>
   );
