a6f2554aa4518a24d587621c1100c93f5445d955
jazztine <jzztn.crz@gmail.com>
2022-12-19 19:49:18 +0800

    style(search): edit ui styles

--- core/components/sections/navigation/search.tsx
+++ core/components/sections/navigation/search.tsx
@@ -2,12 +2,12 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
 const Search = () => {
   return (
-    <div className="clearance flex w-full items-center gap-2 overflow-hidden rounded border bg-white">
+    <div className="clearance flex w-full items-center gap-2 overflow-hidden rounded bg-white">
       <MagnifyingGlassIcon className="icon" />
       <input
         type="text"
         placeholder="Search here..."
-        className="w-full bg-transparent focus:outline-none"
+        className="w-full py-2 bg-transparent focus:outline-none"
       />
     </div>
   );
