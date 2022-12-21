f0f4d9221ce4cd9788d4b7f4a7ebe3c0a7013a4c
jazztine <jzztn.crz@gmail.com>
2022-12-22 00:59:09 +0800

    chore(routes):  move navbar routes to navbar folder

---
+++ core/components/sections/navbar/routes.ts
@@ -0,0 +1,54 @@
+const routes = [
+  {
+    name: "Home",
+    href: "#",
+    isPrivate: false,
+  },
+  {
+    name: "Explore",
+    href: "#",
+    isPrivate: false,
+  },
+  {
+    name: "About Us",
+    href: "#",
+    isPrivate: false,
+  },
+  {
+    name: "Profile",
+    href: "#",
+    isPrivate: true,
+  },
+  {
+    name: "Notifications",
+    href: "#",
+    isPrivate: true,
+  },
+  {
+    name: "Messages",
+    href: "#",
+    isPrivate: true,
+  },
+  {
+    name: "Saved Gigs",
+    href: "#",
+    isPrivate: true,
+  },
+  {
+    name: "Invite a Friend",
+    href: "#",
+    isPrivate: true,
+  },
+  {
+    name: "Settings",
+    href: "#",
+    isPrivate: true,
+  },
+  {
+    name: "Logout",
+    href: "#",
+    isPrivate: true,
+  },
+];
+
+export default routes;
