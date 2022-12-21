202430b2f62ec2cc08e24a45291d851bbdcbcd39
jazztine <jzztn.crz@gmail.com>
2022-12-21 19:37:06 +0800

    style(user layout): create a layout for non-verified users

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,4 +1,4 @@
-import Navigation from "@core/components/sections/navigation";
+import NonVerifiedNavigationBar from "@core/components/sections/navigation/non-verified";
 import Providers from "@core/providers";
 
 type Props = {
@@ -8,11 +8,12 @@ type Props = {
 const Layout = ({ children }: Props) => {
   return (
     <Providers>
+      {/* layout for non verified users*/}
       <div>
         <header className="sticky top-0 z-50 bg-primary-light shadow">
-          <Navigation />
+          <NonVerifiedNavigationBar />
         </header>
-        <main>
+        <main className="grid grid-rows-[auto,1fr] gap-2">
           <section className="bg-gray-500">{children}</section>
         </main>
       </div>
