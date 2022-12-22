f0bec3f21606d3a13fe2b1f9b0d1dd3379763f8c
jazztine <jzztn.crz@gmail.com>
2022-12-22 18:58:25 +0800

    feature(sigin): improve signin ui

--- core/components/modals/sign-in/index.tsx
+++ core/components/modals/sign-in/index.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import Button from "@core/components/elements/button";
+import Logo from "@core/components/elements/logo";
 import Modal from "@core/components/layouts/modal";
 import useModal from "@core/hooks/use-modal";
 import { signIn } from "next-auth/react";
+import Image from "next/image";
 
 const SignIn = () => {
   const { state, handleOpen, handleClose } = useModal();
@@ -15,16 +17,23 @@ const SignIn = () => {
       </Button>
 
       <Modal
-        title="Welcome Back"
+        title={<Logo />}
         state={state}
         handleClose={handleClose}
-        className="max-w-md">
+        className="grid max-w-md gap-2">
         <button
           onClick={() => signIn("google")}
-          className="clearance w-full rounded border">
-          Google
+          className="clearance smooth flex w-full cursor-pointer items-center justify-center gap-4 rounded border hover:-translate-y-1">
+          <Image
+            src="/images/logo/google.png"
+            alt="google logo"
+            width={24}
+            height={24}
+            className="object-cover"
+          />
+          <span>Sign in with Google</span>
         </button>
-        <Button variant="secondary" onClick={handleClose}>
+        <Button variant="primary" onClick={handleClose}>
           Close
         </Button>
       </Modal>
